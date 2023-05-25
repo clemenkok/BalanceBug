a5ab5790dec09cbb3d8749fcfa790aa10de9ee6c
Clemen Kok <68755138+clemenkok@users.noreply.github.com>
2023-05-25 17:27:37 +0100

    mysql nodejs connection established

--- website/server.js
+++ website/server.js
@@ -16,13 +16,20 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// call sysc()
+const db = require("./models");
+db.sequelize.sync();
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bogo node application." });
 });
 
+// tutorial routes
+require("./routes/tutorial.routes.js")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-});   
+});       
