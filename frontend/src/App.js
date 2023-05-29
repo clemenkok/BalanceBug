f34940377ae17c26c65de74a10675ea477f40dde
Clemen Kok <68755138+clemenkok@users.noreply.github.com>
2023-05-29 06:10:33 +0100

    test code

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MatrixComponent from '././matrixcomponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Matrix Viewer</h1>
+      <MatrixComponent />
+    </div>
+  );
+};
+
+export default App;
