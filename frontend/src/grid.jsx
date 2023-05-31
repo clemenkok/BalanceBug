2b2c1205500beb54a10c6da9ba1b006eca1c7963
Clemen Kok <68755138+clemenkok@users.noreply.github.com>
2023-05-31 11:51:02 +0100

    modified 3d array

--- frontend/src/grid.jsx
+++ frontend/src/grid.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
-import { useState } from 'react';
+/*
 
+TODO: (1) Implement new 3D Array grid structure that indicates information 
+
+*/
+
+
+import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+/*
 function generateRandomMatrix(rows, cols) {
   const matrix = [];
   
@@ -19,31 +28,57 @@ function generateRandomMatrix(rows, cols) {
 const rows = 50;
 const cols = 50;
 const randomMatrix = generateRandomMatrix(rows, cols);
+*/
+
+
 
 const Grid = () => {
 
-  const [grid, setGrid] = useState(randomMatrix
-  );
-  console.log(grid);
+  const [matrix, setMatrix] = useState([]);
+
+  useEffect(() => {
+    const fetchMatrix = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/matrix');
+        const { matrix } = response.data;
+        console.log(matrix);
+        setMatrix(matrix);
+      } catch (error) {
+        console.error('Error fetching matrix:', error);
+      }
+    };
+
+    const pollingInterval = setInterval(fetchMatrix, 3000); // Poll every 3 seconds
+
+    return () => {
+      clearInterval(pollingInterval); // Clean up the interval on component unmount
+    };
+
+  }, []);
+
   return (
     <div class="flex items-center justify-center h-screen">
     <div class="text-center">
     <div>
-      {grid.map((row, rowIndex) => (
+      {matrix.map((row, rowIndex) => (
         <div key={rowIndex} className="flex">
-          {row.map((side, colIndex) => (
-            <div
-              key={colIndex}
-              style={{
-                width: '10px',
-                height: '10px',
-                backgroundColor: 'WhiteSmoke',
-                borderTop:  side == 1 ? '2px solid black' : '1px solid white',
-                borderBottom: side == 2 ?  '2px solid black' : '1px solid white',
-                borderLeft: side == 3 ?'2px solid black' : '1px solid white',
-                borderRight: side == 4 ? '2px solid black' : '1px solid white',
-              }}
-            />
+          {row.map((side, sideIndex) => (
+              <div key={sideIndex} className="flex">
+                {side.map((element, infoIndex) => (
+                  <div
+                    key={infoIndex}
+                    style={{
+                      width: '15px',
+                      height: '15px',
+                      backgroundColor: 'WhiteSmoke',
+                      borderTop:  element == 1 ? '2px solid black' : '1px solid white',
+                      borderBottom: element == 2 ?  '2px solid black' : '1px solid white',
+                      borderLeft: element == 3 ?'2px solid black' : '1px solid white',
+                      borderRight: element == 4 ? '2px solid black' : '1px solid white',
+                    }}
+                  />
+                ))}
+              </div>
           ))}
         </div>
       ))}
