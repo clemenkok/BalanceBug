caa9072db6cb3931d006a499cc3d16ba1497434b
Clemen Kok <68755138+clemenkok@users.noreply.github.com>
2023-06-11 01:18:58 +0100

    minor changes in rpi code

--- rpi/capacitor.py
+++ rpi/capacitor.py
@@ -1,95 +1,108 @@
-# import modules
-import ujson
-import urequests
+import network, utime, machine
 import time
-import network  
-from machine import Pin
-import _thread
-import socket
+import ubinascii
+from umqtt.simple import MQTTClient
+import machine
+import random
 
-# PIN allocation (TODO: edit pin alloc)
-led = Pin(15, Pin.OUT)
+# WIFI Credentials
+SSID = "myhotspot"
+SSID_PASSWORD = "racemicracemate"
 
-# Connect to network
-def connect_to_wifi():
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
+# Connect to Wifi
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, SSID_PASSWORD)
+        while not sta_if.isconnected():
+            print("Attempting to connect....")
+            utime.sleep(1)
+    print('Connected! Network config:', sta_if.ifconfig())
+    
+print("Connecting to your wifi...")
+do_connect()
 
-        # fix static ip
-        wlan.ifconfig(('192.168.25.157', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
+# MQTT Credentials
+mqtt_server = '3.92.216.113'
+client_id = 'DC Grid Capacitor'
+user_t = 'BalanceBug'
+password_t = '123'
+topic_pub = 'capacitor_monitoring'
+topic_sub = 'capacitor_control'
+keep_alive = 30
 
-        # network name (ssid) and password:
-        ssid = "Pixel_7155"
-        password = "hello123"
-        wlan.connect(ssid, password)
+last_message = 0
+message_interval = 5
+counter = 0
 
-        # check for connection
-        while wlan.isconnected() == False:
-                print('Waiting for connection...')
-                time.sleep(1)
-        ip = wlan.ifconfig()[0]
-        print(f'Connected on {ip}')
+# Connect to MQTT Broker
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, user=user_t, password=password_t, keepalive=60)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
 
-# send grid data to API endpoint, thread 1
-def send_grid_data():
-        while True:
-                # TODO: code here to obtain relevant grid data (energy, voltage etc)
-                grid_data = ujson.dumps({"voltage": "3V"})
-                url = "http://192.168.25.131:8080/api/live_database" # note check Wi-Fi interface IP address for my PC
-                response = urequests.post(url, headers = {'content-type': 'application/json'}, data=grid_data)
-                print (response.text)
-                time.sleep(5)
+# Reconnect to MQTT Broker
+def reconnect():
+    print('Failed to connected to MQTT Broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
+    
+# Obtain grid power
+def grid_power():
+    print('obtained grid power')
 
-# receive commands from server to charge / discharge capacitor, thread 2
+    # TODO: Write code to obtain the current grid power
+    current_power = random.randint(1,10)
+    return current_power
 
-def control_capacitor():
-        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1] # listen for all ip addresses (need to check if this works for multiple devices)
-        s = socket.socket()
-        s.bind(addr)
-        s.listen(1)
-        print('listening on', addr)
+# Callback to subscribed topic
+def new_message_callback(topic, msg):
+    topic , msg=topic.decode('ascii') , msg.decode('ascii')
+    print("Topic: "+topic+" | Message: "+msg)
+    if msg == '1':
+        # TODO: Insert code to charge capacitor here
+        print("Capacitor Discharging!")
+    elif msg == '0':
+        # TODO: Insert code to discharge capacitor here
+        print("Capacitor Charging!")
+    else:
+        print("Incorrect Message Received!")
 
-        # listening for incoming requests on the socket
-        while True:
-                try:
-                        # we accept incoming connections based off the ip address we are listening on
-                        cl, addr = s.accept()
-                        print('client connected from', addr)
-                        request = cl.recv(1024)
-                        # converts the request data into a string
-                        request = str(request)
-                        print(request)
-                        
-                        # this checks the request string, finding whether "charge" / "discharge" is inside, and returns its position
-                        charge = request.find("charge") # returns index of first occurrence of charge within the request string
-                        discharge = request.find("discharge") # returns index of first occurrence of discharge within the request string
-                        
-                        # so if charge was found, its integer position is given. else if it was not found, -1 would be returned from .find(), thus triggering this
-                        if charge != -1:	# found instruction to charge
-                                print("charge")
-                                led.value(1) # charges (TODO: replace this with the code to charge)
-                              
-                        # likewise for discharge
-                        if discharge != -1:     # found discharge
-                                print("discharge")
-                                led.value(0) # discharges (TODO: replace this with the code to discharge)
+# Connect to broker and subscribe to topics
+try:
+    print('connecting to MQTT Broker')
+    client = mqtt_connect()
+    client.set_callback(new_message_callback)
+    client.subscribe(topic_sub.encode('utf-8'))
 
-                        cl.send('OK')
-                        cl.close()
+except OSError as e:
+    reconnect()
 
-                except OSError as e:
-                        cl.close()
-                        print('connection closed')
+last_message=time.time()
+
+# Loop to publish repeatedly
+while True:
+    try:
+        
+        # every 10 seconds, send capacitor data to server
+        current_power = grid_power()
+        client.publish(topic_pub, msg=str(current_power)) 
+        print('published current grid power:' + str(current_power))
+        time.sleep(10)
+        
+        # check for updates to subscribed topic
+        client.check_msg()
+        
+        # keep alive
+        time.sleep(0.001)
+        if (time.time() - last_message) > keep_alive:
+              client.publish(topic_sub, "Keep alive message")
+              client.publish(topic_pub, "Keep alive message")
+              last_message = time.time()
+    except OSError as e:
+        reconnect()
 
 
-
-# main loop ----------------------------------------------------
-
-# connect to wifi
-connect_to_wifi()
-
-# thread 1: continually push data to server
-first_thread = _thread.start_new_thread(send_grid_data, ())
-
-# thread 2: receive data and charge/discharge capacitor
-control_capacitor()
