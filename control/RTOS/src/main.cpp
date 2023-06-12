7525c3e0be3a01b51b8a30af69c92d46a35e9a9f
Clemen Kok <68755138+clemenkok@users.noreply.github.com>
2023-06-12 13:24:22 +0100

    added task for publishing deadreckoning data

--- control/RTOS/src/main.cpp
+++ control/RTOS/src/main.cpp
@@ -8,7 +8,7 @@
 #define MQTT_USERNAME "BalanceBug"
 #define MQTT_PASSWORD "123"
 
-const char * MQTT_SERVER = "44.201.248.191";
+const char *MQTT_SERVER = "44.201.248.191";
 uint16_t MQTT_PORT = 1883;
 
 WiFiClient wifiClient;
@@ -22,71 +22,101 @@ void connectToMQTT()
   // create clientID
   String clientID = "ESP32Client-";
   clientID += String(random(0xffff), HEX);
-  while ( !MQTTclient.connected() )
+  while (!MQTTclient.connected())
   {
-    MQTTclient.connect( clientID.c_str(), MQTT_USERNAME, MQTT_PASSWORD );
-    Serial.println( "connecting to MQTT" );
-    vTaskDelay( 250 / portTICK_PERIOD_MS ); //task runs approx every 250 mS
+    MQTTclient.connect(clientID.c_str(), MQTT_USERNAME, MQTT_PASSWORD);
+    Serial.println("connecting to MQTT");
+    vTaskDelay(250 / portTICK_PERIOD_MS); // task runs approx every 250 mS
   }
   Serial.println("MQTT Connected");
 }
 
 void WiFiEvent(WiFiEvent_t event)
 {
-  switch (event) {
-    case SYSTEM_EVENT_STA_CONNECTED:
-      Serial.println("Connected to access point");
-      break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-      Serial.println("Disconnected from WiFi access point");
-      break;
-    case SYSTEM_EVENT_AP_STADISCONNECTED:
-      Serial.println("WiFi client disconnected");
-      break;
-    default: break;
+  switch (event)
+  {
+  case SYSTEM_EVENT_STA_CONNECTED:
+    Serial.println("Connected to access point");
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    Serial.println("Disconnected from WiFi access point");
+    break;
+  case SYSTEM_EVENT_AP_STADISCONNECTED:
+    Serial.println("WiFi client disconnected");
+    break;
+  default:
+    break;
   }
 }
 
 void connectToWiFi()
 {
-  Serial.println( "connect to wifi" );
-  while ( WiFi.status() != WL_CONNECTED )
+  Serial.println("connect to wifi");
+  while (WiFi.status() != WL_CONNECTED)
   {
     WiFi.disconnect();
-    WiFi.begin( WIFI_NETWORK, WIFI_PASSWORD );
-    log_i(" waiting on wifi connection" );
-    vTaskDelay( 4000 / portTICK_PERIOD_MS );
+    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+    log_i(" waiting on wifi connection");
+    vTaskDelay(4000 / portTICK_PERIOD_MS);
   }
-  Serial.println( "Connected to WiFi" );
-  WiFi.onEvent( WiFiEvent );
+  Serial.println("Connected to WiFi");
+  WiFi.onEvent(WiFiEvent);
 }
 
 // connect to and keep MQTT connection alive
 void keepMQTTAlive(void *parameters)
 {
   // setting must be set before a mqtt connection is made
-  MQTTclient.setKeepAlive( 90 ); // setting keep alive to 90 seconds makes for a very reliable connection, must be set before the 1st connection is made.
+  MQTTclient.setKeepAlive(90); // setting keep alive to 90 seconds makes for a very reliable connection, must be set before the 1st connection is made.
   for (;;)
   {
-    //check for a is-connected and if the WiFi 'thinks' its connected, found checking on both is more realible than just a single check
-    if ( (wifiClient.connected()) && (WiFi.status() == WL_CONNECTED) )
+    // check for a is-connected and if the WiFi 'thinks' its connected, found checking on both is more realible than just a single check
+    if ((wifiClient.connected()) && (WiFi.status() == WL_CONNECTED))
     {
-      xSemaphoreTake( sema_keepMQTTAlive, portMAX_DELAY ); // whiles MQTTlient.loop() is running no other mqtt operations should be in process
+      xSemaphoreTake(sema_keepMQTTAlive, portMAX_DELAY); // whiles MQTTlient.loop() is running no other mqtt operations should be in process
       MQTTclient.loop();
-      xSemaphoreGive( sema_keepMQTTAlive );
+      xSemaphoreGive(sema_keepMQTTAlive);
     }
-    else {
-      log_i( "MQTT keep alive found MQTT status %s WiFi status %s", String(wifiClient.connected()), String(WiFi.status()) );
-      if ( !(WiFi.status() == WL_CONNECTED) )
+    else
+    {
+      log_i("MQTT keep alive found MQTT status %s WiFi status %s", String(wifiClient.connected()), String(WiFi.status()));
+      if (!(WiFi.status() == WL_CONNECTED))
       {
         connectToWiFi();
       }
       connectToMQTT();
     }
-    vTaskDelay( 250 / portTICK_PERIOD_MS ); //task runs approx every 250 mS
+    vTaskDelay(250 / portTICK_PERIOD_MS); // task runs approx every 250 mS
   }
-  vTaskDelete ( NULL );
+  vTaskDelete(NULL);
 }
+
+void publishDeadreckoningData (void *parameters)
+{
+  // await MQTT Connection
+  while ( !MQTTclient.connected() )
+  {
+    vTaskDelay ( 250 / portTICK_PERIOD_MS );
+  }
+
+  for (;;)
+  {
+    const char * topicDistance = "deadreckoning_data";
+    double distance = 4.0;
+    xSemaphoreTake( sema_keepMQTTAlive, portMAX_DELAY );
+    if ( MQTTclient.connected() )
+    {
+      MQTTclient.publish( topicDistance, String(distance).c_str() );
+      vTaskDelay( 2 / portTICK_PERIOD_MS ); // give MQTT Broker time to receive and process msg
+    }
+    xSemaphoreGive( sema_keepMQTTAlive ); // return Semaphore to keepMQTTAlive
+    vTaskDelay ( 1000 * 15 / portTICK_PERIOD_MS );
+  }
+
+  vTaskDelete (NULL); // TODO: Identify why we have to delete Task
+
+}
+
 
 void setup()
 {
@@ -95,7 +125,7 @@ void setup()
 
   // Keep Alive can mess with MQTT Publish, thus requiring us to use a semaphore.
   sema_keepMQTTAlive = xSemaphoreCreateBinary();
-  xSemaphoreGive( sema_keepMQTTAlive );
+  xSemaphoreGive(sema_keepMQTTAlive);
 
   // task to connect to, and periodically check on MQTT/Wi-Fi
   xTaskCreatePinnedToCore(        // This pins the task to a fixed core
@@ -103,9 +133,20 @@ void setup()
       "Keep MQTT Alive",          // Name of task * for debugging *
       5000,                       // Stack size (bytes)
       NULL,                       // Parameter Passed
-      1,                          // Priority
+      3,                          // Priority
       NULL,                       // Task handle
       CONFIG_ARDUINO_RUNNING_CORE // Fix to running core
+  );
+
+  // task to obtain data from sensors, and publish to MQTT topic
+  xTaskCreatePinnedToCore(
+      publishDeadreckoningData,
+      "Publish Deadreckoning Data",
+      5000,
+      NULL,
+      4,
+      NULL,
+      CONFIG_ARDUINO_RUNNING_CORE
   );
 }
 
