1b871a1feb2cc65fc2622adbd0108149c6839211
Yomna Mohamed <yomihamody@gmail.com>
2023-06-12 13:12:55 +0100

    updates to mapping algo

--- mapping/randm.py
+++ mapping/randm.py
@@ -33,7 +33,7 @@
 import math
 
 # global variables:
-grid_dimensions = None
+grid_dimensions = [5, 5]
 grid_resolution = 1
 sampling_time = None
 
@@ -57,6 +57,7 @@ class Maze:
             for _ in range(grid_dimensions[1]):
                 row.append([False, False, False, False, False])
             self.grid.append(row)
+        self.visitedGrid = self.grid
 
     def fillGrid(self, x, y):
         if (x < 0 or x >= grid_dimensions[0]) or (y < 0 or y >= grid_dimensions[1]) or self.visitedGrid[x][y][0]:
@@ -69,14 +70,14 @@ class Maze:
             nx, ny = x + dx, y + dy
             # check the cell exists and that it's accessible from the current cell
             if (nx >= 0 and nx < grid_dimensions[0]) and (ny >= 0 and ny < grid_dimensions[1]) and not self.visitedGrid[x][y][i+1]:
-                self.fillGrid(self, nx, ny)
+                self.fillGrid(nx, ny)
 
     def updateGrid(self):
         self.visitedGrid = self.grid
         for x in range(grid_dimensions[0]):
             for y in range(grid_dimensions[1]):
                 if not self.visitedGrid[x][y][0]:
-                    self.fillGrid(self, x, y)
+                    self.fillGrid(x, y)
 
     def explored(self):
         for x in range(grid_dimensions[0]):
@@ -90,30 +91,35 @@ maze = Maze()
 
 # helper functions
 def getNeighbours(maze, x, y):
-    maze = maze.grid
+    mazeGrid = maze.grid
     neighbours = [None, None, None, None]
     i = 0
     for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
         nx, ny = x + dx, y + dy
         if (nx >= 0 and nx < grid_dimensions[0]) and (ny >= 0 and ny < grid_dimensions[1]):
-            neighbours[i] = [[nx, ny], maze[nx][ny]]
+            neighbours[i] = [[nx, ny], mazeGrid[nx][ny]]
         i += 1
 
-def nextTarget(maze, x, y, targetList):
+    return neighbours
+
+def nextTarget(maze, x, y, targetList, visited = None):
+    if visited == None:
+        visited = set()
+    visited.add((x, y))
     cell = maze.grid[x][y]
     targetFound = False
-    targetList = []
     neighbours = getNeighbours(maze, x, y)
     nextNeighbours = []
     for i in range(4):
         if neighbours[i] and not (cell[i+1] or neighbours[i][1][0]):
-            nx, ny = neighbours[0][0], neighbours[0][1]
+            nx, ny = neighbours[i][0][0], neighbours[i][0][1] # DONE. TO-DO: CHECK, was neighbours[0][0][0], neighbours[0][1][1]
             nextNeighbours = getNeighbours(maze, nx, ny)
             # this following for loop makes sure that the target cell has neighbour that has been visited
             # i.e. it is moving along hte pseudo walls
-            for i in range(4):
-                if nextNeighbours[i] and nextNeighbours[1][0]:
+            for j in range(4):
+                if nextNeighbours[j] and nextNeighbours[j][1][0]: # DONE. TO-DO: CHECK, was if nextNeighbours[j] and nextNeighbours[1][0]:
                     targetFound = True
+                    print()
                     targetList.append(neighbours[i][0])
                     # we need the following break so that we take the first target that comes up
                     # this is to ensure we always take the first neighbour in the following order:
@@ -123,20 +129,25 @@ def nextTarget(maze, x, y, targetList):
                 break
 
     if targetFound:
-        return targetList
+        return targetFound, targetList
     
     oldNeighbours = neighbours
     for i in range(len(oldNeighbours)):
-        targetList.append(oldNeighbours[i][0])
-        # while not targetFound:
-        x, y = oldNeighbours[i][0][0], oldNeighbours[i][0][1]
-        targetFound, targetList = nextTarget(maze, x, y, targetList)
-        if targetFound:
-            break
-        else:
-            targetList.pop()
+        if oldNeighbours[i]:
+            x, y = oldNeighbours[i][0][0], oldNeighbours[i][0][1]
+            # cell = maze.grid[x][y]
+            if not cell[i+1] and (x, y) not in visited:
+                # print("Old neighbours [i][0]")
+                # print(oldNeighbours[i][0])
+                targetList.append(oldNeighbours[i][0])
+                # while not targetFound:
+                targetFound, targetList = nextTarget(maze, x, y, targetList, visited)
+                if targetFound:
+                    break
+                else:
+                    targetList.pop()
 
-    return targetList
+    return targetFound, targetList
 
 # def mazeExplored(grid):
 #     for r in range(grid_dimensions):
@@ -158,8 +169,8 @@ def updatePosition(distance, bearing):
 
     # convert to grid coordinates
     # not sure if this is needed
-    rover.grid_x = int(rover.x / grid_resolution)
-    rover.grid_y = int(rover.y / grid_resolution)
+    rover.grid_x = int(round(rover.x / grid_resolution))
+    rover.grid_y = int(round(rover.y / grid_resolution))
 
 def appxBearing(bearing):
     # i want to approximate each bearing i get into one of the following sets:
@@ -176,7 +187,7 @@ def appxBearing(bearing):
         if bearing < sector:
             return sectors[sector]
         
-    return 0
+    return [0]
 
 # case a
 def caseA(data):
@@ -199,7 +210,7 @@ def caseA(data):
                     maze.grid[x][ny][2] = True
             if right_wall:
                 maze.grid[x][y][2] = True  # mark right border as wall
-                ny = y+1 # mark right neighbour's left border as true
+                ny = y+1 # mark right neighbour's left border as True
                 if ny >= 0 and ny < grid_dimensions[1]:
                     maze.grid[x][ny][4] = True
         elif bearing == 90:
@@ -216,7 +227,7 @@ def caseA(data):
         elif bearing == 180:
             if left_wall:
                 maze.grid[x][y][2] = True  # mark right border as wall
-                ny = y+1 # mark right neighbour's left border as true
+                ny = y+1 # mark right neighbour's left border as True
                 if ny >= 0 and ny < grid_dimensions[1]:
                     maze.grid[x][ny][4] = True 
             if right_wall:
@@ -255,22 +266,28 @@ def caseB(position, bearing):
 
 # case c: command rover how much and where to move
 def caseC():
-    targetList = nextTarget(maze) 
-
+    targetList = []
+    targetFound, targetList = nextTarget(maze, rover.grid_x, rover.grid_y, targetList) 
     pathCells = len(targetList)
     if pathCells == 0:
         sendCommand("No reachable unvisited cell found", False)
+        return
+    
     for cell in targetList:
         dx = cell[0] - rover.x
         dy = cell[1] - rover.y
         distance = math.sqrt(dx**2 + dy**2)
         bearing = math.atan2(dy, dx)
-        sendCommand(distance, bearing)
+        # when you send command, check if you should break the loop or not
+        # loop to be broken if a wall is found and that takes priority
+        sendCommand(distance, bearing) 
 
         # update stored inputs
         rover.x, rover.y = cell[0], cell[1]
         rover.grid_x, rover.grid_y = cell[0], cell[1]
         maze.grid[cell[0]][cell[1]][0] = True
+
+        # update visited grid
         maze.updateGrid()
     # Calculate the bearing and distance to the next target.
     # dx = next_x - rover.x
@@ -295,4 +312,65 @@ def start():
         elif data['type'] == 'command_request':
             caseC()
 
-start()
+# start()
+
+mazeGrid = [
+    [[False, False, True, False, False], [False, False, False, False, True], [False, False, False, False, False], [False, False, False, False, False], [False, False, False, False, False]],
+    [[False, False, True, False, False], [False, False, False, True, True], [False, False, False, False, False], [False, False, False, False, False], [False, False, False, False, False]],
+    [[False, False, False, False, False], [False, True, True, False, False], [False, False, False, False, True], [False, False, False, True, False], [False, False, False, False, False]],
+    [[False, False, False, False, False], [False, False, True, False, False], [False, False, True, True, True], [False, True, False, False, True], [False, False, False, False, False]],
+    [[False, False, False, False, False], [False, False, False, False, False], [False, True, False, False, False], [False, False, False, False, False], [False, False, False, False, False]]
+]
+
+mazeMap = Maze()
+mazeMap.grid = mazeGrid
+# print(getNeighbours(mazeMap, 2, 1))
+mazeMap.grid[0][0][0] = True
+mazeMap.grid[1][0][0] = True
+mazeMap.grid[2][0][0] = True
+mazeMap.grid[2][1][0] = True
+mazeMap.grid[3][1][0] = True
+mazeMap.grid[4][1][0] = True
+mazeMap.grid[4][2][0] = True
+mazeMap.grid[4][3][0] = True
+mazeMap.grid[3][3][0] = True
+mazeMap.grid[3][4][0] = True
+mazeMap.grid[2][4][0] = True
+mazeMap.grid[2][3][0] = True
+mazeMap.grid[2][2][0] = True
+mazeMap.grid[3][2][0] = True
+mazeMap.grid[1][2][0] = True
+mazeMap.grid[1][1][0] = True
+mazeMap.grid[0][1][0] = True
+targetList = []
+# print(getNeighbours(mazeMap, 0, 1))
+print(nextTarget(mazeMap, 3, 2, targetList))
+# mazeMap.grid[3][2][0] = True
+targetList = []
+# mazeMap.grid[2][2][0] = True
+# mazeMap.grid[1][2][0] = True
+# mazeMap.grid[0][2][0] = True
+print(nextTarget(mazeMap, 0, 2, targetList))
+targetList = []
+# mazeMap.grid[0][3][0] = True
+print(nextTarget(mazeMap, 0, 3, targetList))
+targetList = []
+# mazeMap.grid[0][4][0] = True
+print(nextTarget(mazeMap, 0, 4, targetList))
+targetList = []
+mazeMap.grid[1][4][0] = True
+print(nextTarget(mazeMap, 1, 4, targetList))
+targetList = []
+mazeMap.grid[1][3][0] = True
+print(nextTarget(mazeMap, 1, 3, targetList))
+targetList = []
+mazeMap.grid[2][4][0] = True
+mazeMap.grid[3][4][0] = True
+mazeMap.grid[4][4][0] = True
+print(nextTarget(mazeMap, 4, 4, targetList))
+targetList = []
+mazeMap.grid[4][0][0] = True
+print(nextTarget(mazeMap, 4, 0, targetList))
+targetList = []
+mazeMap.grid[3][0][0] = True
+print(nextTarget(mazeMap, 3, 0, targetList))
