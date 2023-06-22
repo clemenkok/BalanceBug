cdea8e889a9036f151deff4ccddfd6956b5d0ca0
Shermaine <shermaineang2802@gmail.com>
2023-06-22 17:04:37 +0800

    balanced on the spot

--- balancebug_main/balancebug_main.ino
+++ balancebug_main/balancebug_main.ino
@@ -2,6 +2,9 @@
 #include "stepper_motor_control.hpp"
 #include "controller.hpp"
 
+const int MS_PIN1 = 4;
+const int MS_PIN2 = 15;
+const int MS_PIN3 = 27;
 #define INTERVAL_MS_PRINT 200
 #define OUTER_VELO_LOOP_ON 0
 
@@ -9,8 +12,14 @@
 LowPassFilter wheelSpeedFilter = LowPassFilter(0.001, millis()); // Tf = 1ms (filter time constant)
 double filteredWheelSpeed = 0;
 
-LowPassFilter rollFilter = LowPassFilter(0.25, millis()); // Tf = 1ms (filter time constant)
-double filteredRoll = 0;
+int speed = 0;
+double currRoll = 0.0;
+int count = 0;
+double veloControllerOutput;
+double currentVelo;
+
+// LowPassFilter rollFilter = LowPassFilter(0.25, millis()); // Tf = 1ms (filter time constant)
+// double filteredRoll = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,6 +27,9 @@ void setup() {
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
+  pinMode(MS_PIN1, OUTPUT);
+  pinMode(MS_PIN2, OUTPUT);
+  pinMode(MS_PIN3, OUTPUT);
   imuSetup();
   stepperMotorSetup();
   controllerSetup();
@@ -33,7 +45,8 @@ void loop() {
   
   updateImuReadings();
   
-  if (currentMillis - lastPrintMillis > INTERVAL_MS_PRINT && OUTER_VELO_LOOP_ON) {
+  //if (currentMillis - lastPrintMillis > INTERVAL_MS_PRINT && OUTER_VELO_LOOP_ON) {
+  if (OUTER_VELO_LOOP_ON){
     // update the velo PID (have to change the rate of this controller)
   
     // compute the current velo based on wheel speed
@@ -43,16 +56,27 @@ void loop() {
     // curr velo = motorspeed/200*21.2
 
     double currentMotorSpeedInSteps = filteredWheelSpeed;
-    double currentVelo =  currentMotorSpeedInSteps / 200 * 21.2;
-    Serial.println(currentMotorSpeedInSteps);
-    //Serial.println(currentVelo);
-    
+    //double currentMotorSpeedInSteps = speed;
+    currentVelo =  currentMotorSpeedInSteps / 200 * 21.2;
+    // Serial.println("current motor speed in steps");
+    // Serial.println(currentMotorSpeedInSteps);
+    // Serial.println("current velocity");
+    // Serial.println(currentVelo);
     updateVeloInput(currentVelo);
 
     // This should already be covered under the controller
-    double veloControllerOutput = computeVeloControllerOutput();
+    veloControllerOutput = computeVeloControllerOutput();
     setRollSetpoint(veloControllerOutput);
+
+
+
+
+
+    // Serial.println("roll setpoint from veloPID");
+    // Serial.println(veloControllerOutput);
   }
+
+
   // if (currentMillis - lastPrintMillis > INTERVAL_MS_PRINT){
   //   // update the roll PID
   //   filteredRoll = rollFilter(getRoll(), millis());
@@ -63,24 +87,113 @@ void loop() {
   //   setMotorSpeedAngularVelo(-motorSpeedAngularVelo, 2);
   // }
 
-  filteredRoll = rollFilter(getRoll(), millis());
+  //filteredRoll = rollFilter(getRoll(), millis());
   //updateRollInput(filteredRoll);
-  updateRollInput(getRoll());
-  double motorSpeedAngularVelo = computeRollControllerOutput(); //
+
+
+  // veloPID will output rollPID setpoint
+  // rollPID input will be currRoll
+  // 
+
+  currRoll = getRoll();
+  updateRollInput(currRoll);
+  // Serial.print("ROLL: ");
+  // Serial.println(currRoll);
+  
+  double motorSpeedSteps = computeRollControllerOutput(); 
   //Serial.println(filteredRoll);
-  Serial.println(getRoll());
+  //Serial.println(getRoll());
   //Serial.println(getError());
 
-  
-  setMotorSpeedAngularVelo(motorSpeedAngularVelo, 1);
-  //Serial.println(motorSpeedAngularVelo);
-  setMotorSpeedAngularVelo(motorSpeedAngularVelo, 2);
+  // Serial.print("motorspeedsteps: ");
+  // Serial.println(motorSpeedSteps);
+  if (abs(motorSpeedSteps) <= 50){ // move in 16th steps
+  //if (abs(currRoll-setpoint)<=3){
+    // configure le three pins
+    digitalWrite(MS_PIN1, HIGH);
+    digitalWrite(MS_PIN2, HIGH);
+    digitalWrite(MS_PIN3, HIGH);
+    speed = motorSpeedSteps * 16;
 
+  }
+  else if (abs(motorSpeedSteps) <= 100){
+    // configure three pins to move in 8th steps
+    speed = motorSpeedSteps * 8;
+    digitalWrite(MS_PIN1, HIGH);
+    digitalWrite(MS_PIN2, HIGH);
+    digitalWrite(MS_PIN3, LOW);
+  }
+  else if (abs(motorSpeedSteps) <= 200){
+    // configure three pins to move in quarter steps
+    speed = motorSpeedSteps * 4;
+    digitalWrite(MS_PIN1, LOW);
+    digitalWrite(MS_PIN2, HIGH);
+    digitalWrite(MS_PIN3, LOW);
+  }
+  else if (abs(motorSpeedSteps) <= 400){
+    // configure three pins to move in half steps
+    speed = motorSpeedSteps * 2;
+    digitalWrite(MS_PIN1, HIGH);
+    digitalWrite(MS_PIN2, LOW);
+    digitalWrite(MS_PIN3, LOW);
+  }
+  else{ // case where speed is really fast
+    speed = motorSpeedSteps;
+    digitalWrite(MS_PIN1, LOW);
+    digitalWrite(MS_PIN2, LOW);
+    digitalWrite(MS_PIN3, LOW);
+  }
+  
+  //Serial.println(speed);
+  // if (speed<0){
+  //   simpleSetSpeed(speed-1000,1);
+  //   simpleSetSpeed(speed-1000,2);
+  //   digitalWrite(MS_PIN1, HIGH);
+  //   digitalWrite(MS_PIN2, HIGH);
+  //   digitalWrite(MS_PIN3, LOW);
+  // }
+  // else{
+  //   simpleSetSpeed(speed, 1);
+  //   //Serial.println(motorSpeedAngularVelo);
+  //   simpleSetSpeed(speed, 2);
+  // }
+  simpleSetSpeed(speed, 1);
+  // //Serial.println(motorSpeedAngularVelo);
+  simpleSetSpeed(speed, 2);
+  // Serial.println(currRoll);
   runstepperMotors();
-  //delay(10);
+  
+  // if (currentMillis - lastPrintMillis > 200 ){
+  //   Serial.println("turn?");
+  //   turningMotors(speed, 1);
+  // }
+
 
   // put the current wheel speed into the LPF
-  //filteredWheelSpeed = wheelSpeedFilter(getMotorSpeedInSteps(), millis());
+  filteredWheelSpeed = wheelSpeedFilter(motorSpeedSteps, millis());
+
+  if (count >= 20){
+    Serial.print("veloInput, veloOutput/roll setpoint, rollInput, rollOutput, speed: ");
+    Serial.print(currentVelo);
+    Serial.print(", ");
+    Serial.print(veloControllerOutput);
+    Serial.print(", ");
+    Serial.print(currRoll);
+    Serial.print(", ");
+    Serial.print(motorSpeedSteps);
+    Serial.print(", ");
+    Serial.println(speed);
+
+    // Serial.print(potVal);
+    // Serial.print(", ");
+    // Serial.print(Kp);
+    // Serial.print(", ");
+    // Serial.print(Ki);
+    // Serial.print(", ");
+    // Serial.println(Kd);
+    count = 0;
+  }
+  count++;
   
   
 }
--- balancebug_main/stepper_motor_control.cpp
+++ balancebug_main/stepper_motor_control.cpp
@@ -1,17 +1,22 @@
 #include "stepper_motor_control.hpp"
 #include <AccelStepper.h> 
+#include <FastAccelStepper.h>
 
 // Define stepper motor connections
-#define STEP_PIN1 26 
-#define DIR_PIN1 32 
-#define STEP_PIN2 33 
-#define DIR_PIN2 25
+#define STEP_PIN1 32 
+#define DIR_PIN1 33 
+#define STEP_PIN2 25 
+#define DIR_PIN2 26
 
-#define STEPS_PER_REV 200*16
+#define STEPS_PER_REV 200
 
 // Create an instance of AccelStepper
 AccelStepper stepper1(1, STEP_PIN1, DIR_PIN1);
 AccelStepper stepper2(1, STEP_PIN2, DIR_PIN2);
+// FastAccelStepperEngine engine1 = FastAccelStepperEngine();
+// FastAccelStepperEngine engine2 = FastAccelStepperEngine();
+// FastAccelStepper *stepper1 = NULL;
+// FastAccelStepper *stepper2 = NULL;
 
 double stepSpeedToAngularVelo(double stepsPerSec){
   // converts steps per sec to radians per sec
@@ -23,14 +28,36 @@ double angularVeloToStepSpeed(double angularVelo){
 }
 
 void stepperMotorSetup() {
-  // Set the maximum speed and acceleration
+  //Set the maximum speed and acceleration
   stepper1.setMaxSpeed(1001);     // Set the maximum speed in steps per second
   stepper2.setMaxSpeed(1001);     // Set the maximum speed in steps per second
   
-  // Set the initial speed
+  //Set the initial speed
   stepper1.setSpeed(100);         // Set the constant speed in steps per second
   stepper2.setSpeed(-100);         // Set the constant speed in steps per second
+  /*
+  engine1.init();
+  engine2.init();
+  stepper1 = engine1.stepperConnectToPin(STEP_PIN1);
+  stepper2 = engine2.stepperConnectToPin(STEP_PIN2);
+  if (stepper1) {
+    stepper1->setDirectionPin(DIR_PIN1);
+    stepper1->setEnablePin(1);
+    stepper1->setAutoEnable(true);
+    stepper1->setSpeedInUs(1000); }
+  if (stepper2) {
+    stepper2->setDirectionPin(DIR_PIN2);
+    stepper2->setEnablePin(1);
+    stepper2->setAutoEnable(true);
+    stepper2->setSpeedInUs(1000); }
 
+  stepper1->setSpeedInHz(1001); // Set the maximum speed in Hz
+  stepper2->setSpeedInHz(1001); // Set the maximum speed in Hz
+
+  //Set the initial speed
+  stepper1->setSpeedInHz(100);  // Set the constant speed in Hz
+  stepper2->setSpeedInHz(-100); // Set the constant speed in Hz
+*/
 }
 
 void setMotorSpeedAngularVelo(double angularVelo, int motorNumber){
@@ -40,17 +67,47 @@ void setMotorSpeedAngularVelo(double angularVelo, int motorNumber){
   }
   else if (motorNumber == 2){
     stepper2.setSpeed(-angularVeloToStepSpeed(angularVelo));
+    //stepper2->setSpeedInHz(-angularVeloToStepSpeed(angularVelo));
+  }
+}
+
+void simpleSetSpeed(double angularVelo, int motorNumber){
+  if (motorNumber == 1){
+    stepper1.setSpeed(angularVelo);
+    //Serial.println(-angularVeloToStepSpeed(angularVelo));
+  }
+  else if (motorNumber == 2){
+    stepper2.setSpeed(-angularVelo);
+    //stepper2->setSpeedInHz(-angularVeloToStepSpeed(angularVelo));
   }
 }
 
 void runstepperMotors(){
   stepper1.run();
   stepper2.run();
+  // stepper1->runForward();
+  // stepper2->runForward();
 }
 
 double getMotorSpeedInSteps(){
   return stepper1.speed();
 }
+
+void turningMotors(double angularVelo, int motorNumber){
+  if (motorNumber == 1){
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(0);
+      stepper1.setSpeed(angularVelo);
+  }
+  else if (motorNumber == 2){
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(0);
+      stepper2.setSpeed(angularVelo);
+  }
+  stepper1.run();
+  stepper2.run();
+}
+
 
 /*
 void loop() {
