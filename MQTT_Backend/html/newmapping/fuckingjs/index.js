263618ba1e65fec071b402d91d99bd0f3143e4d1
jieunboy0516 <106169847+jieunboy0516@users.noreply.github.com>
2023-06-24 02:17:42 +0100

    5 step

--- MQTT_Backend/html/newmapping/fuckingjs/index.js
+++ MQTT_Backend/html/newmapping/fuckingjs/index.js
@@ -638,7 +638,7 @@ wallselect.addEventListener("change", function (event) {
 
 //simulator******************************************************
 
-let simulatorstep = 4;
+let simulatorstep = 5;
 
 document.addEventListener("keydown", function (event) {
   if (
